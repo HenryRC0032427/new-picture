dec7f7b016653f13a3718cbb1b3b6aa266bdb015
Henry Winckles <rc0032427@student.riversidecollege.ac.uk>
2018-09-18 10:01:13 +0100

    path - sqaure

--- new picture/Form1.Designer.cs
+++ new picture/Form1.Designer.cs
@@ -47,9 +47,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(343, 206);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(119, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "pathway";
+            this.button1.Text = "pathway square";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
